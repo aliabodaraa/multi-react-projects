530aabf65b3d57b75ffe1505dc6fa65fd2f5f242
Ali Abodaraa <abodaraaali50@gmail.com>
2023-03-25 22:11:10 +0200

    solve the problem in multiple bind ways that is wrap the reference for handleSubmit with arrow in component invokation also you can solve it by replace the normal handle function with arrow function or bind it directly in the invoker component on in the constructer that is up to you :(

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,6 @@ import React from "react";
 import axios from 'axios'
 import SearchBar from "./SearchBar"
 export default class App extends React.Component {
-  constructor(props){
-    super(props);
-  }
   state={images:[]};
   async handleSubmit(term){
     let response=await axios.get("https://api.unsplash.com/search/photos",{
@@ -13,16 +10,22 @@ export default class App extends React.Component {
         Authorization: "Client-ID pkU1oiB38nbIjBJ9dSxkREozQxUy13ltnwSmMoUajFw"
       }
     });
-    //this.setState({images:response.data.results});
+    this.setState({images:response.data.results});
     console.log(this);//the result is parentMethod : {async handleSubmit,guessIamI="guessIamI"}
   }
+
   render(){
-    return ( 
+    return (
       <div className="ui container" style={{ marginTop:"10px" }}>
-        <SearchBar parentMethod={this.handleSubmit} guessIamI="guessIamI"/>
+        <SearchBar parentMethod={this.handleSubmit.bind(this)}/>
         catch : {this.state.images.length }
       </div>
     );
   }
 }
+//xolve the binding issue :
+// 1.replace normal function with `arrow` let handleSubmit=async(term)=>{...}
+// 2. `bind` this.handleSubmit in constructor this.handleSubmit=this.handleSubmit.bind(this)
+// 3.`bind` this.handleSubmit in where you invoke it <SearchBar parentMethod={this.handleSubmit.bind(this)}/>
+// 3. wrap the child component(that you invoked ) with arrowFunction <SearchBar parentMethod={()=>this.handleSubmit.bind(this)(arguments)}/>
  
