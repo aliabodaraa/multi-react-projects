27b7a1d2a24c59ccff33ff1674fd4a63a1231349
Ali Abodaraa <abodaraaali50@gmail.com>
2023-05-25 13:27:48 +0300

    lodash package solution1 to decrease many requests , the downside is when the user change still momoize

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,22 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder';
+import _ from 'lodash';
 
 export const fetchPosts = () => async dispatch => {
-  const response = await jsonPlaceholder.get('/posts');
+    const response = await jsonPlaceholder.get('/posts');
 
-  dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
+
+export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+const _fetchUser = _.memoize(async(id, dispatch) => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
+});
+export const fetchUserInefficient = function(id) { //the downside of this approch is everytime you will invoke fetchUser() you will create async function in memory and memoize it and call it from redux-thunk
+    return _.memoize(async function(dispatch) {
+        const response = await jsonPlaceholder.get(`/users/${id}`);
+
+        dispatch({ type: 'FETCH_USER', payload: response.data });
+    });
+}
