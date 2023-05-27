4188e97d8f272a220182c84378451e61d1f419c0
Ali Abodaraa <abodaraaali50@gmail.com>
2023-05-27 15:16:30 +0300

    solution2 to decrease many requests Action creator inside Action creator

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,24 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder';
 import _ from 'lodash';
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPostsAndUsers = () => async(dispatch, getState) => {
+    await dispatch(fetchPosts());
+    //console.log(getState());
+
+    //const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    //userIds.forEach(id => dispatch(fetchUser(id)));
+
+    //refactor loadash with _.chain
+    _.chain(getState().posts).map('userId').uniq().forEach(userId => dispatch(fetchUser(userId))).value();
+
+
+    //if you want to use the users result from network request use the following because you can't use async await syntax inside `forEach`
+    //1.await Promise.all(userIds.map(id => dispatch(fetchUser(id)))); 
+    //console.log(getState());
+    //or 
+    //2.//Promise.all(userIds.map(id => dispatch(fetchUser(id)))).then((r) => console.log(getState()));
+
+};
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPlaceholder.get('/posts');
@@ -7,16 +26,15 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-const _fetchUser = _.memoize(async(id, dispatch) => {
+export const fetchUser = id => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type: 'FETCH_USER', payload: response.data });
-});
-export const fetchUserInefficient = function(id) { //the downside of this approch is everytime you will invoke fetchUser() you will create async function in memory and memoize it and call it from redux-thunk
-    return _.memoize(async function(dispatch) {
-        const response = await jsonPlaceholder.get(`/users/${id}`);
+};
 
-        dispatch({ type: 'FETCH_USER', payload: response.data });
-    });
-}
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// });
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import thunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(reducers, applyMiddleware(thunk));//applyMiddleware(thunk) any time you dispatch an action the action will first send to the redux-thunk as a middleware after that send to other different reducers
 
 ReactDOM.render(
   <Provider store={store}>
