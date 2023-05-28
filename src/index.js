56e6024054eeaa502793a341ee22ad07d71a3b5d
Ali Abodaraa <abodaraaali50@gmail.com>
2023-05-28 15:32:55 +0300

    make navigation using react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 
 import App from './components/App';
-import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));//applyMiddleware(thunk) any time you dispatch an action the action will first send to the redux-thunk as a middleware after that send to other different reducers
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.querySelector('#root')
-);
+ReactDOM.render(<App />, document.querySelector('#root'));
