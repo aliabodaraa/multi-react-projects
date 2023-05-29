b106dcc0312fd29cb098d1812d2da6eea015f96a
Ali Abodaraa <abodaraaali50@gmail.com>
2023-05-29 09:36:47 +0300

    Handling Authentication

--- src/components/App.js
+++ src/components/App.js
@@ -10,16 +10,14 @@ import Header from './Header';
 const App = () => {
   return (
     <div className="ui container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Routes>
+      <BrowserRouter><Header />
+      <Routes>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
           <Route path="/streams/delete" exact component={StreamDelete} />
-          <Route path="/streams/show" exact component={StreamShow} /></Routes>
-        </div>
+          <Route path="/streams/show" exact component={StreamShow} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
